40f64419ad6359a84fa6c52908b15d71af97471b
Gabriele31201 <gabri31201@gmail.com>
2023-03-29 16:58:03 +0200

    add card in index

--- js/main.js
+++ js/main.js
@@ -23,17 +23,19 @@ let prezzoTicketDiscount40 = prezzoTicket / 100 * 60
 
 if(eta < 18){
   console.log("Si applica lo sconto del 20%, il totale è quindi:" + prezzoTicketDiscount20);
-
+    document.getElementById("prezzobiglietto").innerHTML = prezzoTicketDiscount20.toFixed(2)+"&euro;";
 }else if (eta > 65){
-    console.log("Si applica lo sconto del 40%, il totale è quindi:" + prezzoTicketDiscount40);
+   console.log("Si applica lo sconto del 40%, il totale è quindi:" + prezzoTicketDiscount40);
+    document.getElementById("prezzobiglietto").innerHTML = prezzoTicketDiscount40.toFixed(2)+"&euro;";
 }else{
-    console.log("Prezzo standard, il costo è di:" + prezzoTicket)
+   console.log("Prezzo standard, il costo è di:" + prezzoTicket)
+    document.getElementById("prezzobiglietto").innerHTML = prezzoTicket.toFixed(2)+"&euro;";
 }
 
 
-document.getElementById("prezzobiglietto").innerHTML = prezzoTicket
 
-document.getElementById("sconto").innerHTML = prezzoTicketDiscount20
-document.getElementById("sconto").innerHTML = prezzoTicketDiscount40
+
+
+
 
 
